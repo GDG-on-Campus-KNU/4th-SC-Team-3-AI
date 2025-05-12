ea20b02c0675de90d2fd031c875661a22a4c5a31
scv1702 <scv1702@naver.com>
2025-05-12 17:58:21 +0900

    feat: 배포 스크립트 작성

--- Dockerfile
+++ Dockerfile
@@ -1,24 +1,13 @@
-# 1. Python Slim 베이스 이미지
-FROM python:3.10-slim
+FROM python:3.13.2-slim
 
-# 2. 작업 디렉토리 설정
 WORKDIR /app
 
-# 3. 종속성 설치용 파일 복사
-COPY requirements.txt .
+COPY . /app
 
-# 4. 종속성 설치
-RUN pip install --upgrade pip && \
-    pip install --no-cache-dir -r requirements.txt
-# 5. 프로젝트 코드 복사 (모델은 제외)
-COPY ./app ./app
-COPY main.py .
+EXPOSE $PORT
 
-# 6. 모델 디렉토리는 외부 마운트 예정
-VOLUME ["/ke_t5_natural_prompt2dict_model"]
+ENV PYTHONUNBUFFERED 1
 
-# 6. 포트 오픈
-EXPOSE 8000
+RUN python3 -m pip install --no-cache-dir -r requirements.txt
 
-# 7. FastAPI 서버 실행
-CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8000"]
+CMD ["python3", "main.py"]
--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 # main.py
 from fastapi.responses import JSONResponse
-import uvicorn
 from fastapi import FastAPI, Request
 from app.exceptions.gemini_exception import GeminiException
 from app.routes.analyze import router as analyze_router
 from app.routes.generate import router as generate_router
+from dotenv import load_dotenv
+
+import uvicorn
+import os
 
 app = FastAPI()
 
@@ -22,4 +25,6 @@ async def general_exception_handler(request: Request, exc: GeminiException):
     )
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    load_dotenv()
+    port = int(os.getenv("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
